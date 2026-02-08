7b4f3144b98d3c6cdd2f6ee9259790f83c63f10b
James Pinder <jhpinder@gmail.com>
2026-02-08 16:25:23 -0500

    init

---
+++ src/constants.h
@@ -0,0 +1,5 @@
+#include <Adafruit_TinyUSB.h>
+#include <Arduino.h>
+#include <MIDI.h>
+
+#define MIDI_RCV_BUFFER_SIZE 512
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "UsbInterface.h"
+
+Adafruit_USBD_MIDI usbMidi;
+
+MIDI_CREATE_INSTANCE(Adafruit_USBD_MIDI, usbMidi, MIDI);
+
+void setup() {}
+
+void loop() {}
